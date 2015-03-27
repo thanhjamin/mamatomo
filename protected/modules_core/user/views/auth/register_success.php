e10a5170b02c70daeabf8610c6ae5fcc5d4ca8e4
UlyssesLin <utemoc@gmail.com>
2015-03-27 00:47:23 -0700

    3/27 12:47am: Changed register_success message and added login link

--- protected/modules_core/user/views/auth/register_success.php
+++ protected/modules_core/user/views/auth/register_success.php
@@ -16,11 +16,13 @@
     <br/>
     <div class="row">
         <div class="panel panel-default" style="max-width: 300px; margin: 0 auto 20px; text-align: left;">
-            <div class="panel-heading"><?php echo Yii::t('UserModule.views_auth_register_success', '<strong>Registration</strong> successful!'); ?></div>
+            <div class="panel-heading"><?php echo Yii::t('UserModule.views_auth_register_success', '<strong>Welcome</strong> to the mamatomo family!'); ?></div>
             <div class="panel-body">
-                <p><?php echo Yii::t('UserModule.views_auth_register_success', 'Please check your email and follow the instructions!'); ?></p>
+                <p><?php echo Yii::t('UserModule.views_auth_register_success', 'Before we move on, let\'s get you fully signed up!'); ?></p>
+                <p><?php echo Yii::t('UserModule.views_auth_register_success', 'Please check your email and click the <strong class="nowrap">Sign up</strong> link we\'ve sent you to complete registration.'); ?></p>
                 <br/>
-                <a href="<?php echo $this->createUrl('//') ?>" class="btn btn-primary"><?php echo Yii::t('UserModule.views_auth_register_success', 'back to home') ?></a>
+                <a href="<?php echo $this->createUrl('//') ?>" class="btn btn-primary"><?php echo Yii::t('UserModule.views_auth_register_success', 'Back to home') ?></a>
+                <a href="<?php echo $this->createUrl('//user/auth/recoverPassword') ?>" class="btn btn-primary"><?php echo Yii::t('UserModule.views_auth_register_success', 'Log In') ?></a>
             </div>
         </div>
     </div>
